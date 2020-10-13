8ec992249b5cd7b343cc15ce47d2770abd22ee25
Carmela Leung <carmela.leung@shopify.com>
2020-10-13 09:48:22 -0400

    Make template

--- index.js
+++ index.js
@@ -36,41 +36,19 @@ async function getInfo() {
 
 async function generateReadMe() {
   await getInfo(url);
-  let new_file = `
-[<img alt='${name}' src='${img_src}' width=100 align='right'>](${link})
-<p align='right'>Today, ${text_month} ${day}, is</p>
-<p align='right'><b>${name}</b> <em>(via <a href='https://daysoftheyear.com'>Days of the Year</a>)</em></p>
 
-## :wave: Hello World, I'm Carmela
-[<img alt='Shopify logo' src='assets/shopify-logo.png' width=15>](https://www.shopify.com/) Dev Degree Intern @ Shopify || Computer Science Student @ York University [<img alt='York University logo' src='assets/york-logo.jpg' width=15>](https://www.yorku.ca/)
-
-│⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀█⠀⠀│  ⠀♬⠀⠀⠀⠀⠀
-│⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀█⠀⠀│  ⠀⠀⠀⠀⠀⠀
-│⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀│⠀⠀█⠀⠀█⠀⠀█⠀⠀│  ⠀⠀⠀⠀⠀♪⠀
-│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│  ⠀⠀⠀⠀
-│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│  ⠀⠀♩
-│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀⠀│⠀⠀│  ⠀⠀⠀⠀
-¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯  ⠀⠀⠀⠀
-⠀♬⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀♪♪⠀⠀⠀⠀⠀⠀⠀⠀⠀♩⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀♫♫⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀♬⠀⠀⠀⠀⠀⠀⠀♪♫
-
-* :sparkling_heart: Pronouns: she/her
-* :world_map: Based in Canada
-* :computer: Currently working on [Shopify CLI](https://github.com/Shopify/shopify-app-cli)
-* :seedling: Trying to learn Node, GitHub Actions
-* :card_index_dividers: Side projects: both my GitHub READMEs
-* :speech_balloon: Interested in robotics, accessibility
-* :love_you_gesture: Fun fact: I'm learning ASL
-
-****
-
-[<img align='left' alt='LinkedIn badge' src='https://img.shields.io/badge/-Carmela%20Leung-2867B2?style=for-the-badge&logo=linkedin&link=https://www.linkedin.com/in/carmela-leung-50919b14b/'/>](https://www.linkedin.com/in/carmela-leung-50919b14b/)
-[<img align='left' alt='My Other GitHub Badge' src='https://img.shields.io/badge/-carmelore-2b3137?style=for-the-badge&logo=github&link=https://github.com/carmelore'/>](https://github.com/carmelore)
-[<img align='left' alt='Facebook Badge' src='https://img.shields.io/badge/-Carmela%20Leung-3b5998?style=for-the-badge&logo=facebook&logoColor=white&link=https://facebook.com/pastelswirlsmusic'/>](https://facebook.com/pastelswirlsmusic) <p align='right'> made with ♥️  + <img alt='JavaScript logo' src='assets/js-logo.png' width=15></p>
-  `;
-
-  fs.writeFile('README.md', new_file, function(err) {
+  await fs.readFile('template.txt', 'utf-8', function(err, data) {
     if (err) throw err;
-    console.log('ah h  h h h hh h hhh h');
+    data = data.replace(/{{name}}/g, name);
+    data = data.replace(/{{link}}/g, link);
+    data = data.replace(/{{img_src}}/g, img_src);
+    data = data.replace(/{{text_month}}/g, text_month);
+    let new_readme = data.replace(/{{day}}/g, day);
+
+    fs.writeFile('README.md', new_readme, function(err) {
+      if (err) throw err;
+      console.log('ah h  h h h hh h hhh h');
+    });
   });
 }
 
